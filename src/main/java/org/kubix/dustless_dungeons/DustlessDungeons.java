7ddbbb4b3572b569cb1015c21ad0cb1679e7b290
kubas <kubastu05@gmail.com>
2025-04-14 13:41:34 +0200

    The test

---
+++ src/main/java/org/kubix/dustless_dungeons/DustlessDungeons.java
@@ -0,0 +1,17 @@
+package org.kubix.dustless_dungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DustlessDungeons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
