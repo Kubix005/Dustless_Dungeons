9c4fa6b7a43d6f1cfde1717323ae2d935db08b54
kubas <kubastu05@gmail.com>
2025-04-14 13:46:57 +0200

    The test v2

--- src/main/java/org/kubix/dustless_dungeons/DustlessDungeons.java
+++ src/main/java/org/kubix/dustless_dungeons/DustlessDungeons.java
@@ -6,7 +6,7 @@ public final class DustlessDungeons extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        System.out.println("Say hi to git");
 
     }
 
